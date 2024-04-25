e4e06e6e3c6eff0dfd7ab9cdb5460d25c368d9e8
Soden46 <syarifsoden22@gmail.com>
2024-04-26 01:13:39 +0700

    Add Whatsapp Send Message

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('/', function () {
     return view('undangan.index');
 });
 
-Route::get('/taufik-citra', [WhatsAppController::class, 'index'])->name('handle.whatsapp.parameter');
+Route::get('/taufik-citra', [WhatsAppController::class, 'handleWhatsAppParameter'])->name('handle.whatsapp.parameter');
 
 Route::get('/wa', [WhatsAppController::class, 'showWhatsAppForm'])->name('send.whatsapp.form');
 Route::post('/send-invitations', [WhatsAppController::class, 'sendWhatsAppInvitations'])->name('send.invitations');
