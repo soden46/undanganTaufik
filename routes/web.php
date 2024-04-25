93ee7d674621d9f2cfe45d4a6fab509251de6ef3
Soden46 <syarifsoden22@gmail.com>
2024-04-26 00:01:41 +0700

    Add Whatsapp Send Message

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('/', function () {
     return view('undangan.index');
 });
 
-Route::get('/taufik-citra', function () {
+Route::get('/taufik-citra?to=', function () {
     return view('undangan.index');
 });
 
