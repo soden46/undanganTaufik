ddfb3655899ecf239bc945adbab9b35223fed0bc
Soden46 <syarifsoden22@gmail.com>
2024-04-26 22:33:59 +0700

    Add Tampilan Acara Mempelai Wanita

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [WhatsAppController::class, 'handleWhatsAppParameter']);
 
+Route::get('/taufik-citra-from-taufik', [WhatsAppController::class, 'handleWhatsAppParameter'])->name('handle.whatsapp.parameter');
 Route::get('/taufik-citra', [WhatsAppController::class, 'handleWhatsAppParameter'])->name('handle.whatsapp.parameter');
 
 Route::get('/wa', [WhatsAppController::class, 'showWhatsAppForm'])->name('send.whatsapp.form');
 Route::post('/send-invitations', [WhatsAppController::class, 'sendWhatsAppInvitations'])->name('send.invitations');
 
 Route::get('/citra', [WhatsAppController::class, 'handleWhatsAppParameterCitra'])->name('handle.whatsapp.parameter.citra');
-Route::get('/citra/taufik-citra', [WhatsAppController::class, 'handleWhatsAppParameterCitra'])->name('handle.whatsapp.parameter.citra');
+Route::get('/taufik-citra-from-citra', [WhatsAppController::class, 'handleWhatsAppParameterCitra'])->name('handle.whatsapp.parameter.citra');
 Route::get('/citra/wa', [WhatsAppController::class, 'showWhatsAppFormCitra'])->name('send.whatsapp.form.citra');
 Route::post('/citra/send-invitations', [WhatsAppController::class, 'sendWhatsAppInvitationsCita'])->name('send.invitations.citra');
