1ee2a0f2541e3342f31d4baba5b9809bad7ea1cd
Soden46 <syarifsoden22@gmail.com>
2024-04-26 21:40:12 +0700

    Add Tampilan Acara Mempelai Wanita

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ Route::get('/taufik-citra', [WhatsAppController::class, 'handleWhatsAppParameter
 Route::get('/wa', [WhatsAppController::class, 'showWhatsAppForm'])->name('send.whatsapp.form');
 Route::post('/send-invitations', [WhatsAppController::class, 'sendWhatsAppInvitations'])->name('send.invitations');
 
+Route::get('/citra', [WhatsAppController::class, 'handleWhatsAppParameter'])->name('handle.whatsapp.parameter.citra');
 Route::get('/citra/taufik-citra', [WhatsAppController::class, 'handleWhatsAppParameter'])->name('handle.whatsapp.parameter.citra');
 Route::get('/citra/wa', [WhatsAppController::class, 'showWhatsAppFormCitra'])->name('send.whatsapp.form.citra');
 Route::post('/citra/send-invitations', [WhatsAppController::class, 'sendWhatsAppInvitationsCita'])->name('send.invitations.citra');
