fac7e60a9a23f67a3f8843c12ba428e3fdd54cf5
Soden46 <syarifsoden22@gmail.com>
2024-06-22 22:26:36 +0700

    Update

--- resources/views/undangan/handlepatmi.blade.php
+++ resources/views/undangan/handlepatmi.blade.php
@@ -106,7 +106,7 @@
                 <div class="overflow-x-hidden">
                     <div data-aos="fade-right" data-aos-duration="2000">
                         <div class="img-crop border border-3 border-light shadow my-4 mx-auto">
-                            <img src="./assets/images/11.jpg" alt="taufik" onclick="util.modal(this)" />
+                            <img src="./assets/images/11.webp" alt="taufik" onclick="util.modal(this)" />
                         </div>
                         <h1 class="font-esthetic" style="font-size: 3rem">Taufik Baharsyah</h1>
                         <p class="mt-3 mb-0" style="font-size: 1.25rem">Putra Pertama</p>
@@ -135,7 +135,7 @@
 
                     <div data-aos="fade-left" data-aos-duration="2000">
                         <div class="img-crop border border-3 border-light shadow my-4 mx-auto">
-                            <img src="./assets/images/12.jpg" alt="citra" onclick="util.modal(this)" />
+                            <img src="./assets/images/12.webp" alt="citra" onclick="util.modal(this)" />
                         </div>
                         <h1 class="font-esthetic" style="font-size: 3rem">Citra Dwi Nur Afni Oktafiana</h1>
                         <p class="mt-3 mb-0" style="font-size: 1.25rem">Putri Kedua</p>
@@ -299,13 +299,13 @@
 
                         <div class="carousel-inner rounded-4">
                             <div class="carousel-item active">
-                                <img src="./assets/images/1.jpg" alt="gambar 1" class="d-block w-100" onclick="util.modal(this)" />
+                                <img src="./assets/images/1.webp" alt="gambar 1" class="d-block w-100" onclick="util.modal(this)" />
                             </div>
                             <div class="carousel-item">
-                                <img src="./assets/images/3.jpg" alt="gambar 2" class="d-block w-100" onclick="util.modal(this)" />
+                                <img src="./assets/images/3.webp" alt="gambar 2" class="d-block w-100" onclick="util.modal(this)" />
                             </div>
                             <div class="carousel-item">
-                                <img src="./assets/images/4.jpg" alt="gambar 3" class="d-block w-100" onclick="util.modal(this)" />
+                                <img src="./assets/images/4.webp" alt="gambar 3" class="d-block w-100" onclick="util.modal(this)" />
                             </div>
                         </div>
 
@@ -449,7 +449,7 @@
                 </h1>
 
                 <div class="img-crop border border-3 border-light shadow mb-4 mx-auto">
-                    <img src="./assets/images/10.jpg" alt="bg" />
+                    <img src="./assets/images/10.webp" alt="bg" />
                 </div>
 
                 <h1 class="font-esthetic my-4" style="font-size: 2.5rem">
@@ -481,7 +481,7 @@
     <div class="loading-page" id="loading" style="opacity: 1;">
         <div class="d-flex justify-content-center align-items-center" style="height: 100vh !important;">
             <div class="text-center w-75">
-                <img class="img-fluid mb-3" src="./assets/images/icon-192x192.png" alt="icon" style="width: 3.5rem;">
+                <img class="img-fluid mb-3" src="./assets/images/icon-192x192.webp" alt="icon" style="width: 3.5rem;">
                 <div class="progress" role="progressbar" style="height: 0.5rem;">
                     <div class="progress-bar" id="bar" style="width: 0%"></div>
                 </div>
