afc4fc60a08abf953219e214dca106f7d0b7a1dd
Soden46 <syarifsoden22@gmail.com>
2024-06-23 00:18:31 +0700

    Update

--- resources/views/undangan/handlecitra.blade.php
+++ resources/views/undangan/handlecitra.blade.php
@@ -559,7 +559,7 @@
             <div class="modal-content">
                 <div class="modal-body p-0">
                     <div class="d-flex justify-content-center align-items-center" style="height: 100%">
-                        <img src="./assets/images/bg.jpeg" class="w-100" alt="foto" id="show-modal-image" />
+                        <img src="./assets/images/bg.webp" class="w-100" alt="foto" id="show-modal-image" />
                     </div>
                 </div>
             </div>
